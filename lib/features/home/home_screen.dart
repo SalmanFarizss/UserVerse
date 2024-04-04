2fa39bda218ecbc4d3f1f7063f43b2f421106e1c
SalmanFarizss <faris03salman@gmail.com>
2024-04-04 23:39:35 +0530

    auth section UI:login_screen,OTP_screen

---
+++ lib/features/home/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+// import '../../core/globals.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController searchController=TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+
+    );
+  }
+}
